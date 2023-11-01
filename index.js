0e2d9dfebe76911c8de0d065ea2ac26afee2c325
Gaurav Joshi <gaurav@Gauravs-MacBook-Air.local>
2023-11-01 12:16:43 +0530

    Make Start Screen

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+flag = 1;
+
 const winningCombos = [
 	[0, 1, 2],
 	[3, 4, 5],
@@ -10,13 +12,54 @@ const winningCombos = [
 ];
 
 let b = ["", "", "", "", "", "", "", "", ""]; //Array to store all cell values
+let p1Name, p2Name;
+let winCount = [0, 0]; //0-O 1-X
+// function start(){
+// 	let page = document.getElementById("main");
+// 	page.style.display = "block";
+// }
 
-function reset() { //To reset cells and restart
-	location.reload();
+function start() {
+	const startS = document.getElementById("start-screen");
+	const mainS = document.getElementById("main");
+
+	// Add a click event listener to the Start button
+	startS.style.display = "none";
+	mainS.style.display = "block";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+
+	const form = document.getElementById("players");
+
+	form.addEventListener("players", function (event) {
+		event.preventDefault(); // Prevent the default form submission
+		const formData = new FormData(form);
+		// Access individual form fields by name
+		p1Name = formData.get("p-1");
+		p2Name = formData.get("p-2");
+		console.log(p1Name);
+		console.log(p2Name);
+	});
+
+});
+
+function reset() {
+	//To reset cells and restart
+	start();
+	for (let i = 0; i < 9; i++) {
+		let ele = document.getElementById("b" + (i + 1));
+		ele.disabled = false;
+		ele.value = "";
+		ele.style.color = "black";
+		flag = 1;  
+
+	}
 	b = ["", "", "", "", "", "", "", "", ""];
 }
 
-function lock(x, y, z) { //Function to disable cell once it is used
+function lock(x, y, z) {
+	//Function to disable cell once it is used
 	for (let i = 0; i < 9; i++) {
 		let btn = document.getElementById("b" + (i + 1));
 		if (i != x && i != y && i != z) {
@@ -27,7 +70,9 @@ function lock(x, y, z) { //Function to disable cell once it is used
 	}
 }
 
-function Win(player) { //To check if any winning combination matches
+
+function Win(player) {
+	//To check if any winning combination matches
 	for (const combo of winningCombos) {
 		let [x, y, z] = combo;
 		if (b[x] === player && b[y] === player && b[z] === player) {
@@ -38,7 +83,8 @@ function Win(player) { //To check if any winning combination matches
 	return false;
 }
 
-function checkTie() { //To check if its a tie;
+function checkTie() {
+	//To check if its a tie;
 	for (let i = 0; i < 9; i++) {
 		if (b[i] == "") {
 			return false;
@@ -47,12 +93,9 @@ function checkTie() { //To check if its a tie;
 	return true;
 }
 
-
-flag = 1;
 function tic(n) {
 	let ele = document.getElementById("b" + n);
-	let player = (flag == 1) ? 'X' : 'O';
-	flag = (flag == 1) ? 0 : 1; //ALternate X and O
+	let player = (flag == 1) ? "X" : "O";
 	ele.disabled = true; //lock the cell after value is put
 	ele.value = player; //Put X or O in cell
 
@@ -61,17 +104,13 @@ function tic(n) {
 		b[i] = document.getElementById("b" + (i + 1)).value;
 	}
 
-	let display = document.getElementById("print")
+	let display = document.getElementById("print");
 	
 	if (Win(player)) {
-		display.innerHTML = "Player "+player+" won";
-	} 
-	else if (checkTie()) {
+		display.innerHTML = "Player " + player + " won";
+		winCount[flag]++;
+	} else if (checkTie()) {
 		display.innerHTML = "Match Tie";
-	} 
-	else {
-		display.innerHTML = "Player "+player+" Turn";
 	}
+	flag = flag == 1 ? 0 : 1; //ALternate X and O
 }
-
-
